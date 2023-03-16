0f81ab0faf72d99a4a020e1356a480ff8bb39e79
Şükrü Berke Yapıcı <berke.yapiciii@gmail.com>
2023-03-16 14:05:51 +0300

    update one

--- word-test.py
+++ word-test.py
@@ -10,17 +10,23 @@ df = pd.read_excel(file_name)
 # Toplam puan
 score = 0
 
+# Doğru cevaplanan kelimelerin listesi
+correct_guesses = []
+
 # Oyun döngüsü
 while score < 20:
-    # Rastgele kelime seçin
+    # Rastgele kelime seçin, ancak daha önce doğru cevaplanmamış olmalıdır.
     word = random.choice(df["kelimeler"])
+    while word in correct_guesses:
+        word = random.choice(df["kelimeler"])
     
     # Ekrana kelimeyi yazdırın ve kullanıcıdan tahmin isteyin
-    guess = input(f"What is the English meaning of '{word}'? ")
+    guess = input(f"What is the English meaning of '{word}'? = ")
     
-    # Tahmin doğruysa, puanı artırın
+    # Tahmin doğruysa, puanı artırın ve doğru cevaplanan kelimeler listesine ekleyin.
     if guess.lower() == df.loc[df["kelimeler"]==word, "anlamlar"].values[0].lower():
         score += 1
+        correct_guesses.append(word)
         print("Correct!")
     else:
         print("Incorrect. Try again.")
